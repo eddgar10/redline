219cb791b40006f4cc5d4835da8f89b66fa0f662
Edgar Espinosa <36211850+eddgar10@users.noreply.github.com>
2021-11-10 14:36:11 -0600

    Add files via upload
    modificaciones en CSS

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 document.querySelector('.menu-btn').addEventListener('click',() => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
-
+ScrollReveal({ reset: true });
 ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.showcase', {delay:2000});
-ScrollReveal().reveal('.news-cards', {delay:2000});
-ScrollReveal().reveal('.cards-banner-one', {delay:2000});
-ScrollReveal().reveal('.cards-banner-two', {delay:2000});
+ScrollReveal().reveal('.showcase', {delay:300});
+ScrollReveal().reveal('.news-cards', {delay:200});
+ScrollReveal().reveal('.cards-banner-one', {delay:100});
+ScrollReveal().reveal('.cards-banner-two', {delay:100});
